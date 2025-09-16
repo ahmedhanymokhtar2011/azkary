2572192b0316c1d1fd784aba4529766939c07933
ahmedhanymokhtar2011 <ahmedhmokhtar54@gmail.com>
2025-09-16 22:22:18 +0300

    تعديل جديد على الموقع

--- js/main.js
+++ js/main.js
@@ -78,3 +78,21 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+//back to the top btn
+  const backToTopBtn = document.getElementById("backToTop");
+//when user down 200px
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  });
+
+  //smooth back to the top
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
